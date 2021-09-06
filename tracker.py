751e90cd76410c7a36fe54d60643a76af5548b01
wchim <51901431+wchim@users.noreply.github.com>
2021-09-06 12:52:52 -0400

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -19,7 +19,7 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600, hash_funcs={FooType: hash})
+@st.cache(ttl=600, hash_funcs={pd.DataFrame: lambda _: None})
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
