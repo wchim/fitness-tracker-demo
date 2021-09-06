8e50810497c6c3b2e1aa627e5e93b3e2f0f55735
wchim <51901431+wchim@users.noreply.github.com>
2021-09-06 13:11:33 -0400

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -19,7 +19,8 @@ conn = connect(credentials=credentials)
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600, hash_funcs={_cffi_backend.__CDataGCP: my_hash_func})
+@st.cache(ttl=600,
+          allow_output_mutation=True)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
