f2184698c49827392000400192878c665de0dd20
wchim <51901431+wchim@users.noreply.github.com>
2021-09-06 12:40:08 -0400

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -27,8 +27,8 @@ def run_query(query):
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-days = []
-weights = [] 
+#days = []
+#weights = [] 
 
 # Print results.
 for row in rows:
